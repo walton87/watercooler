78270f0cac12a477ed458e0eef8d5120c0414dae
Gabriel Walton <gabriel.walton@outlook.com>
2013-11-21 20:39:01 +0000

    Created a rail application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watercooler::Application.config.secret_token = '35b13dc6c8891b9ac7cf3b406228d595628729414a37f8addb5d2fb49cf6d7219606a469d5b4fd3d3ddb27a6cb2219e3c9e2db572b3c559df0a20ec223f4b3df'
